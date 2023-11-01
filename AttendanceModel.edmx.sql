7e89acf4dc9c5b90d099b231040c8531b7b588cc
yzdnaufan <ahyaznau@gmail.com>
2023-11-02 01:45:29 +0700

    fix model and MVP

---
+++ AttendanceModel.edmx.sql
@@ -0,0 +1,54 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/02/2023 00:55:40
+-- Generated from EDMX file: G:\VS Repo\ConsoleAppEF\AttendanceModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [AttendanceDB];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [UserId] int IDENTITY(1,1) NOT NULL,
+    [username] nvarchar(max)  NOT NULL,
+    [password] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [UserId] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([UserId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
--- Program.cs
+++ Program.cs
@@ -10,7 +10,45 @@ namespace ConsoleAppEF2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            using (var db = new AttendanceModelContainer())
+            {
+                Console.WriteLine("Masukkan username untuk dimasukkan kedalam database :");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("Masukkan password untuk dimasukkan kedalam database :");
+                var pass = Console.ReadLine();
+
+
+                var user = new Users() { username = name, password = pass };
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                var q = from b in db.Users
+                        orderby b.UserId
+                        select b;
+                Console.WriteLine("Berikut adalah list user yang ada :\nUserId\t\tUsername\tPassword");
+                foreach (var item in q)
+                {
+                    Console.WriteLine(item.UserId + "\t\t" + item.username + "\t\t" + item.password);
+                }
+
+                Console.WriteLine("Silahkan pilih userid yang ingin dihapus :");
+                var i = Int32.Parse( Console.ReadLine());
+
+                user = db.Users.Where(b => b.UserId == i).Single();
+
+                db.Users.Remove(user);
+                db.SaveChanges();
+
+                Console.WriteLine("Berikut adalah list user yang ada :\nUserId\t\tUsername\tPassword");
+                foreach (var item in q)
+                {
+                    Console.WriteLine(item.UserId + "\t\t" + item.username + "\t\t" + item.password);
+                }
+
+                Console.WriteLine("Press any key to end...");
+                Console.ReadKey();
+            }
         }
     }
 }
